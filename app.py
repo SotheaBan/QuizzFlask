e57b0b89cfc80105411de6fe3c88ac4152757481
SotheaBan <bansothea.morakot@gmail.com>
2025-05-20 10:46:06 +0700

    project setting up file

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask,redirect, url_for, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "✅ Flask is running!"
+    return render_template('pages/homepage.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
